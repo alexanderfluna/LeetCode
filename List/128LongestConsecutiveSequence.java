de4e7303d49857790bedbc8b80194bd166d9a115
Alexander Luna <59184968+alexanderfluna@users.noreply.github.com>
2024-07-12 21:21:23 -0400

    Update 128LongestConsecutiveSequence.java

---
+++ List/128LongestConsecutiveSequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        HashSet<Integer> hs = new HashSet<>();
+        for (int num : nums)
+            hs.add(num);
+        int longest = 1;
+        for (int num : nums) {
+
+            if (!hs.contains(num - 1)) { 
+                int count = 1;
+                while (hs.contains(num + 1)) {
+                    num++;
+                    count++;
+                }
+                longest = Math.max(longest, count);
+
+            }
+            if (longest > nums.length / 2)
+                break;
+
+        }
+        return longest;
+    }
+}
